2d6fd180e0b07cf31376030086d9eaf76578b4d6
Ling Samuel <lingsamuel@foxmail.com>
2016-02-23 21:24:42 +0800

    扒到了一堆用户

--- crawler.py
+++ crawler.py
@@ -9,9 +9,9 @@ import types
 import BeautifulSoup
 import MySQLdb
 
-mainURL = "https://www.zhihu.com/"
-topicURL = "https://www.zhihu.com/topic/"
-questionURL = "https://www.zhihu.com/question/"
+mainURL = u"https://www.zhihu.com/"
+topicURL = u"https://www.zhihu.com/topic/"
+questionURL = u"https://www.zhihu.com/question/"
 like = u"赞同:"
 author = u"作者:"
 colon = u":"
@@ -21,13 +21,16 @@ cs = 19580349
 
 
 class LinkGetter:
-    def __init__(self, topic):
+    def __init__(self, topic, error_logger, message_logger, utils):
         self.topic = topic
         self.links = []
         self.title = []
-        self.utils = Utils()
+        self.message_logger = message_logger
+        self.utils = utils
+        self.error_logger = error_logger
         self.pattern = re.compile(
             '<a class="question_link".*?href="/question/(.*?)">(.*?)</a>', re.S)
+        self.message_logger.event(u"Link Getter Built")
 
     def links_crawler(self):
         topic_url = topicURL + str(self.topic)
@@ -51,10 +54,13 @@ class LinkGetter:
 
 
 class AnswerGetter:
-    def __init__(self, links, like_limit):
+    def __init__(self, links, like_limit, error_logger, message_logger, utils):
         self.links = links
         self.limit_like = like_limit
-        self.utils = Utils()
+        self.message_logger = message_logger
+        self.utils = utils
+        self.error_logger = error_logger
+        self.database = Database(self.error_logger, self.message_logger, self.utils)
         self.titlePattern = re.compile(
             '<h2 class="zm-item-title zm-editable-content">(.*?)</h2>', re.S)
         self.detailPattern = re.compile(
@@ -63,7 +69,8 @@ class AnswerGetter:
             # 0: vote, 1: author url, 2: author name, 3: answer content
             '<span class="count">(.*?)</span>.*?<a class="author-link".*?href="/people/(.*?)">(.*?)</a>.*?'
             '<div class="zm-editable-content clearfix">(.*?)</div>', re.S)
-        self.results = []
+        self.results = {}
+        self.message_logger.event(u"Answer Getter Built")
 
     def question_crawler(self):
         for link in self.links:
@@ -74,6 +81,15 @@ class AnswerGetter:
         try:
             content_list = self.get_response(request=request)
             self.print_response(content_list)
+            link_id = link.replace(questionURL, "")
+            self.message_logger.crawl_question(link_id, content_list[0].group(1))
+            self.results = {
+                "question_id": link_id,
+                "title": content_list[0].group(1)
+            }
+
+            self.database.users(content_list[2])
+            self.database.insert_data(self.results, "questions")
         except urllib2.URLError, e:
             self.utils.url_error_handle(e, link)
 
@@ -104,7 +120,7 @@ class AnswerGetter:
 
 
 class Utils:
-    def __init__(self):
+    def __init__(self, error_logger, message_logger):
         # 去除img标签,7位长空格
         self.removeImg = re.compile('<img.*?>|</img>')
         # 删除超链接标签
@@ -119,7 +135,9 @@ class Utils:
         # 将其余标签剔除
         self.removeExtraTag = re.compile('<.*?>')
 
-        self.logger = ErrorLogger()
+        self.message_logger = message_logger
+        self.error_logger = error_logger
+        self.message_logger.event(u"Utils Built")
 
     def replace(self, x):
         x = re.sub(self.removeImg, "", x)
@@ -133,46 +151,56 @@ class Utils:
         return x.strip()
 
     def url_error_handle(self, exception, url):
-        self.logger.url_error(exception, url)
+        self.error_logger.url_error(exception, url)
 
     def mysqldb_error_handle(self, exception):
-        self.logger.database_error(exception)
+        self.error_logger.database_error(exception)
 
     @staticmethod
     def get_current_time():
-        return time.strftime('[%Y-%m-%d %H:%M:%S]', time.localtime(time.time()))
+        return time.strftime(u'[%Y-%m-%d %H:%M:%S]', time.localtime(time.time()))
 
     @staticmethod
     def get_current_data():
-        return time.strftime('%Y-%m-%d', time.localtime(time.time()))
+        return time.strftime(u'%Y-%m-%d', time.localtime(time.time()))
 
 
 class Logger:
     def __init__(self, name):
         self.log_handler = open(name, 'ab+')
-        sys.stdout = self.log_handler
+        # sys.stdout = self.log_handler
 
     def output_line_log(self, message):
         self.log_handler.write(Utils.get_current_time())
-        self.log_handler.write(message)
+        # print type(message)
+        self.log_handler.write(message.encode('utf8'))
         self.log_handler.write('\n')
 
 
-class MessageLogger (Logger):
+class MessageLogger(Logger):
     def __init__(self):
         Logger.__init__(self, 'log.log')
+        self.event(u"Message Logger Built")
+
+    def crawl_question(self, link_id, title):
+        self.output_line_log(u"Question ID:" + link_id + u"Question Title" + title + u"Crawled")
+
+    def event(self, message):
+        self.output_line_log(u"Event:" + message)
 
 
-class ErrorLogger (Logger):
+class ErrorLogger(Logger):
     def __init__(self):
         Logger.__init__(self, 'error.log')
+        self.message_logger = MessageLogger()
+        self.message_logger.event(u"Error Logger Built")
 
     def database_error(self, exception):
-        error_detail = "数据库错误，原因%d: %s" % (exception.args[0], exception.args[1])
+        error_detail = u"数据库错误，原因%d: %s" % (exception.args[0], exception.args[1])
         print error_detail
         self.output_line_log(error_detail)
         if "key 'PRIMARY'" in exception.args[1]:
-            error_detail = "数据已存在"
+            error_detail = u"数据已存在"
             print error_detail
             self.output_line_log(error_detail)
 
@@ -183,31 +211,26 @@ class ErrorLogger (Logger):
         elif hasattr(exception, "reason"):
             print exception.reason
             self.output_line_log(exception.reason)
-        self.output_line_log("request:" + url)
-
-
-class Test:
-    def test(self):
-        topic_link_getter = LinkGetter(topic=cs)
-        topic_link_getter.links_crawler()
-        links_collection = topic_link_getter.get_links()
-        answer_getter = AnswerGetter(links=links_collection, like_limit=100)
-        answer_getter.question_crawler()
+        self.output_line_log(u"request:" + url)
 
 
 class Database:
-    def __init__(self):
-        self.utils = Utils()
+    def __init__(self, error_logger, message_logger, utils):
+        self.message_logger = message_logger
+        self.utils = utils
+        self.error_logger = error_logger
         try:
             self.db = MySQLdb.connect('localhost', 'zhihu', 'crawler', 'zhihu')
             self.cur = self.db.cursor()
             self.db.set_character_set('utf8')
         except MySQLdb.Error, e:
             self.utils.mysqldb_error_handle(exception=e)
+        self.message_logger.event(u"Database Built")
 
     def insert_data(self, data_dict, table):
         try:
             sql = self.insert_sql_construct(data_dict, table)
+            self.execute_insert(sql)
         except MySQLdb.Error, e:
             self.utils.mysqldb_error_handle(e)
 
@@ -227,7 +250,32 @@ class Database:
                 return -1
         except MySQLdb.Error, e:
             self.db.rollback()
+            self.utils.mysqldb_error_handle(e)
+
+    def users(self, contents):
+        for content in contents:
+            # print "Love"
+            # print content[1]
+            sql = 'INSERT INTO users (%s) VALUES (%s)' % ("user_url", '"' + content[1] + '"')
+            self.execute_insert(sql)
+
+
+class Test:
+    def __init__(self):
+        self.message_logger = MessageLogger()
+        self.error_logger = ErrorLogger()
+        self.utils = Utils(self.error_logger, self.message_logger)
+
+    def test(self):
+        topic_link_getter = LinkGetter(cs, self.error_logger, self.message_logger, self.utils)
+        topic_link_getter.links_crawler()
+        links_collection = topic_link_getter.get_links()
+        answer_getter = AnswerGetter(links_collection, 100, self.error_logger, self.message_logger, self.utils)
+        answer_getter.question_crawler()
 
 
 tester = Test()
 tester.test()
+
+# topic_link_getter = LinkGetter(topic=cs)
+# topic_link_getter.links_crawler()
